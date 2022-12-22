80967af3ae2a992b43f526ab1a184a40c22dd496
Ivan.Guo-郭一凡 <Ivan.Guo@fanruan.com>
2022-12-22 16:27:53 +0800

    rabbitmq-TTL

---
+++ springboot-rabbitmq-01/src/main/java/com/rabbitmq/SpringbootRabbitmq01Application.java
@@ -0,0 +1,13 @@
+package com.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmq01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmq01Application.class, args);
+    }
+
+}
